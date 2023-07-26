d8df26db6014fc8642c8cc57213d7120b66e0c2a
Viktor Sívek <sivv01@vse.cz>
2023-07-26 18:21:41 +0200

    works

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Viktor</span></h1>
-<p className={`${styles.heroSubText} mt-2 text-white-100`}>I do web applications and <br className="sm:block hidden" />data analysis mainly in python</p>
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>I do web applications and <br className="sm:block hidden" />data analysis mainly in python</p>
         </div>
       </div>
 
